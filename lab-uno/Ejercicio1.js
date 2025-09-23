f2bf07e76109d243456d1b7efe16877609cc2bf1
AlexGB12 <cguitz-2022434@kinal.edu.gt>
2025-09-23 12:00:27 -0600

    Primer ejercicio resuelto

---
+++ lab-uno/Ejercicio1.js
@@ -0,0 +1,8 @@
+const numero = parseInt(prompt("Ingresa un número para saber si es positivo, negativo o cero"));
+if (numero > 0) {
+    console.log("El número ingresado es Positivo.");
+} else if (numero < 0) {
+    console.log("El número ingresado es Negativo.");
+} else {
+    console.log("El número ingresado es Cero.");
+}
